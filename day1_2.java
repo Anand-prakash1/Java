979f814eedd2bf5f73e6788c4b7a5cc3306a414b
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2024-11-09 23:15:14 +0530

    Update day1_2.java

--- day1_2.java
+++ day1_2.java
@@ -1,5 +1,6 @@
 public class day1_2 {
     public static void main(String[] args) {
+        // pattern printing easily....
         System.out.println("*");
         System.out.println("**");
         System.out.println("***");
