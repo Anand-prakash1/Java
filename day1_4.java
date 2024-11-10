946eb0652e2e7f065ce529b9f61e06128f0c137d
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2024-11-10 20:36:07 +0530

    Add files via upload

---
+++ day1_4.java
@@ -0,0 +1,10 @@
+public class day1_4 {
+    public static void main(String[] args) {
+        // Sum of two number 
+        int a = 30;
+        int b = 48;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
+ 
