7f171802886e12bb67d03de00dfd05ec7c93ef87
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2024-11-10 20:34:37 +0530

    Add files via upload

---
+++ day1_3.java
@@ -0,0 +1,8 @@
+public class day1_3 {
+    public static void main(String[] args) {
+        // variable and data types
+        String name = "Anand prakash";
+        int age = 17;
+        double price = 25.25;
+    }
+}
