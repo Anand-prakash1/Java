eeb0ad39f30c3e35055f179ab52c49b46eef2dc0
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2024-11-13 01:08:43 +0530

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,12 @@
+public class day3 {
+    public static void main(String[] args) {
+        // Initialize num1 with 100, but this initial value is immediately overwritten by the loop
+        int num1 = 100;
+
+        // For loop to print numbers from 0 to 50
+        for(num1 = 0; num1 <= 50; num1++) { 
+            // Print the current value of num1 in each iteration
+            System.out.println(num1);
+        }
+    }
+}
