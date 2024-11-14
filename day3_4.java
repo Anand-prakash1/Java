798fb7b81ff78fb2e7ef965ad14d59e5e6aff4d0
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2024-11-14 23:47:41 +0530

    Add files via upload

---
+++ day3_4.java
@@ -0,0 +1,9 @@
+public class day3_4 {
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            System.out.println("Hello world");
+            i++;
+        }while(i < 10);
+    }
+}
