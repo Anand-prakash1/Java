41d8229fb3fbf1be74bb7eda8948291e0d9f4f3c
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-01-14 02:37:54 +0530

    Add files via upload

---
+++ day3_5.java
@@ -0,0 +1,26 @@
+public class day3_5 {
+    public static void main(String[] args) {
+        // sum of 10 number from 1 to 10 using all loops
+        // uncomment the code and use it
+
+        int n = 1;
+        int sum = 0;
+
+        // while (n <= 10) {
+        //     sum += n;
+        //     n++;
+        // }
+        //   System.out.println("sum of number form 1 to 10 is: " + sum);  
+
+        // for(int i = 0; i <= 10; i++){
+        //     sum += i;
+        // }
+        // System.out.println("sum of number form 1 to 10 is: " + sum);
+
+        // do{
+        //     sum += n;
+        //     n++;
+        //     System.out.println(sum);
+        // }while(n <= 10);
+    }
+}
